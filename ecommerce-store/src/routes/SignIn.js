fe78aa44323ca2b9aad3af4217e31d3d59ffd7f1
Vukipoo <perovicv515@gmail.com>
2023-06-24 10:12:56 -0700

    create sign up component, form component, button component

--- ecommerce-store/src/routes/SignIn.js
+++ ecommerce-store/src/routes/SignIn.js
@@ -1,11 +1,17 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../utils/firebase.utils.js'
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../utils/firebase.utils.js';
+import SignUpForm from '../components/SignUpForm/SignUpForm.js';
+
 
 
 function SignIn() {
+   
     const logGoogleUser = async() => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
+
     }
+
+ 
   return (
     <div>
         <h1>
@@ -14,8 +20,10 @@ function SignIn() {
         <button onClick = {logGoogleUser}>
             Sign in with google PopUp
         </button>
+        <SignUpForm />
+      
     </div>
   )
-}
+  }
 
 export default SignIn
