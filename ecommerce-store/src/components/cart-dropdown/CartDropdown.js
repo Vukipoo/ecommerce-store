6aeb7f23eea7c8c7951ccc1073e3fbb77472c0fe
Vukipoo <perovicv515@gmail.com>
2023-06-25 23:01:20 -0700

    add items to cart logic

--- ecommerce-store/src/components/cart-dropdown/CartDropdown.js
+++ ecommerce-store/src/components/cart-dropdown/CartDropdown.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './CartDropdown.styles.scss'
 import Button from '../Button'
+import CartItem from '../CartItem/CartItem'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/CartContext'
 
-function CartDropdown() {
+const  CartDropdown = () => {
+    const { cartItems } = useContext(CartContext)
   return (
     <div className='cart-dropdown-container'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+            {cartItems.map((item) =>( <CartItem key={item.id} cartItem = {item}/>))}
+        </div>
         <Button>GO TO CHECKOUT</Button>
     </div>
   )
