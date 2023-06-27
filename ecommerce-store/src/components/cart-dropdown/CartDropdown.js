d1fd699d6d00e63a940c42abd97b0b85491f377e
Vukipoo <perovicv515@gmail.com>
2023-06-26 18:36:11 -0700

    create checkout page

--- ecommerce-store/src/components/cart-dropdown/CartDropdown.js
+++ ecommerce-store/src/components/cart-dropdown/CartDropdown.js
@@ -1,29 +1,31 @@
-import React from 'react'
+import React from "react";
 
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-import './CartDropdown.styles.scss'
-import Button from '../Button'
-import CartItem from '../CartItem/CartItem'
-import { useContext } from 'react'
-import { CartContext } from '../../contexts/CartContext'
+import "./CartDropdown.styles.scss";
+import Button from "../Button";
+import CartItem from "../CartItem/CartItem";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
 
-const  CartDropdown = () => {
-    const { cartItems } = useContext(CartContext)
-    const navigate = useNavigate()
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
 
-    const goToCheckoutHandler = () => {
-      navigate('/checkout')
-    }
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
 
   return (
-    <div className='cart-dropdown-container'>
-        <div className='cart-items'>
-            {cartItems.map((item) =>( <CartItem key={item.id} cartItem = {item}/>))}
-        </div>
-        <Button onClick = {goToCheckoutHandler}>GO TO CHECKOUT</Button>
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
-  )
-}
+  );
+};
 
-export default CartDropdown
+export default CartDropdown;
--- ecommerce-store/src/components/checkout/Checkout.js
+++ ecommerce-store/src/components/checkout/Checkout.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import { useContext } from 'react'
-import { CartContext } from '../../contexts/CartContext'
-import './Checkout.styles.scss'
+import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
+import "./Checkout.styles.scss";
+import CheckoutItem from "../checkout-item/CheckoutItem";
 
 function Checkout() {
-    const { cartItems } = useContext(CartContext)
-
+  const { cartItems, cartTotal } =
+    useContext(CartContext);
 
   return (
-    <div>
-        <h1>I be the chekcout page son</h1>
-        <div>
-            {cartItems.map((cartItem) => {
-                    const { id, name, quantity } = cartItem;
-                    return (
-                <div key={id}>
-                        <h2>{name}</h2>
-                        <span>{quantity}</span>
-                        </div>
-                        )
-                })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
         </div>
+        <div className="header-block"><span>Product</span></div>
+        <div className="header-block"><span>Description</span></div>
+        <div className="header-block"><span>Quantity</span></div>
+        <div className="header-block"><span>Price</span></div>
+        <div className="header-block"><span>Remove</span></div>
+      </div>
+        {cartItems.map((cartItem) => 
+           <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+        )}
+        <span className="total">Total: ${cartTotal}</span>
     </div>
-
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
